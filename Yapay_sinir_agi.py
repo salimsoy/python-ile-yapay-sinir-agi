0901dead978fa307addf85916639f442e49f4e6b
salim soy <salimsoy0320@gmail.com>
2025-06-03 11:36:36 +0300

    gereksiz satirlar silindi

--- Yapay_sinir_agi.py
+++ Yapay_sinir_agi.py
@@ -9,22 +9,18 @@ def relu(x):
 def tanh(x):
     return np.tanh(x)
 
-# Kullanıcıdan parametre alalım
-input_vector = np.array([2.5, 3.0])  # Girdi verisi (örnek)
 
-# Gizli katman ağırlıkları (2 giriş -> 3 nöron)
+input_vector = np.array([2.5, 3.0])
+
 weights_input_hidden = np.array([
     [-1.4, 2.3],
     [0.7, -0.5]
 ])
 
-# Gizli katman bias
 bias_hidden = np.array([1.3, 0.9])
 
-# Çıkış katmanı ağırlıkları (3 gizli -> 1 çıkış)
 weights_hidden_output = np.array([[-0.1, -0.3]])
 
-# Çıkış bias
 bias_output = np.array([0.5])
 
 A = np.dot(weights_input_hidden,input_vector.T)
